199905387cc224b526a47163b9301a4547dc7c09
msp <100043909+dev-msp@users.noreply.github.com>
2025-09-20 12:35:21 -0400

    feat: initial commit

---
+++ grammar.js
@@ -0,0 +1,17 @@
+/**
+ * @file Flix grammar for tree-sitter
+ * @author msp
+ * @license MIT
+ */
+
+/// <reference types="tree-sitter-cli/dsl" />
+// @ts-check
+
+module.exports = grammar({
+  name: "flix",
+
+  rules: {
+    // NOTE: add the actual grammar rules
+    changeme: $ => "",
+  }
+});
