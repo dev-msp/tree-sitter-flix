90c0ca0747c4b1dd90797f8631c15aaf4fafd0ba
Matt Palmer <palmer.s.matt@gmail.com>
2025-09-23 08:45:46 -0400

    feat(typing): add JSDoc types, make list helpers accept options, and simplify wrapper helpers
    - Add JSDoc comments for helper functions to improve type information for TypeScript checking
    - Change `sep1`/`sep2`/`commaSep1` signatures to accept an options object `{ trailing?, precedence? }` instead of separate params
    - Update all call sites to remove explicit false trailing args and use new signatures
    - Introduce typed parameter and return annotations in comments for:
      - `moddedSeq`
      - `surr`
      - `sep1`
      - `sep2`
      - `commaSep1`
      - `binary`
      - `makeWrapperOptional`
      - `parens`
      - `brackets`
      - `braces`
    - Move and reimplement `makeWrapperOptional`, `parens`, `brackets`, `braces` after helper definitions to avoid forward-reference errors and to centralize wrappers

--- grammar.js
+++ grammar.js
@@ -7,16 +7,8 @@
 /// <reference types="tree-sitter-cli/dsl" />
 // @ts-check
 
-const makeWrapperOptional =
-  (fn) =>
-  (...rules) =>
-    choice(fn(...rules), seq(...rules));
-
-const parens = (...rules) => surr("(", ")", ...rules);
-const brackets = (...rules) => surr("[", "]", ...rules);
-const braces = (...rules) => surr("{", "}", ...rules);
-
 const parensOptional = makeWrapperOptional(parens);
+const bracketsOptional = makeWrapperOptional(brackets);
 const bracesOptional = makeWrapperOptional(braces);
 
 module.exports = grammar({
@@ -30,6 +22,7 @@ module.exports = grammar({
     [$.expression, $.call_expression],
     [$.binary_expression, $.body],
     [$.body],
+    [$.path],
   ],
 
   precedences: ($) => [
@@ -97,7 +90,7 @@ module.exports = grammar({
         "struct",
         field("name", $.identifier),
         optional(field("type_params", $.type_params)),
-        braces(commaSep1($.struct_field, false)),
+        braces(commaSep1($.struct_field)),
       ),
     struct_field: ($) =>
       seq(
@@ -199,14 +192,18 @@ module.exports = grammar({
         "=",
         field("body", bracesOptional($.body)),
       ),
-    // Use/Import (simplified)
+    _qualified_java_reference: ($) =>
+      seq($.identifier, optional(seq(token.immediate("$"), $.identifier))),
+
+    _import_alias: ($) =>
+      braces(seq($._qualified_java_reference, "=>", $.identifier)),
     use_or_import: ($) =>
       seq(
         choice("use", "import"),
         $.path,
-        optional(seq("as", $.identifier)),
-        optional($.semi),
+        optional(seq(".", choice($.identifier, $._import_alias))),
       ),
+
     // Function parameters
     _fn_parameters: ($) => parens(optional(commaSep1($.fn_parameter))),
     fn_parameter: ($) =>
@@ -281,7 +278,6 @@ module.exports = grammar({
           ),
         ),
       ),
-    // TODO - this should permit more
     tuple: ($) => parens(sep2($.expression, ",", undefined)),
     pipeline_expression: ($) => prec.right(binary($, "|>", $.expression)),
     call_expression: ($) =>
@@ -339,30 +335,25 @@ module.exports = grammar({
       surr("#{", "}", repeat(choice($.datalog_fact, $.datalog_rule))),
     datalog_fact: ($) => seq($.relation_expression, token.immediate(".")),
 
-    query_source: ($) => commaSep1($.identifier, false),
-    query_selection: ($) => parens(commaSep1($.identifier, false)),
+    query_source: ($) => commaSep1($.identifier),
+    query_selection: ($) => parens(commaSep1($.identifier)),
 
     // TODO - make parens "immediate"
     relation_expression: ($) =>
-      seq(field("name", $.identifier), parens(commaSep1($.expression, false))),
-
+      seq(field("name", $.identifier), parens(commaSep1($.expression))),
     datalog_rule: ($) =>
       seq(
         field("head", $.relation_expression),
         ":-",
-        field("body", commaSep1($.relation_expression, false)),
+        field("body", commaSep1($.relation_expression)),
         optional(seq(",", "if", parens(field("condition", $.expression)))),
         ".",
       ),
-
     datalog_type: ($) =>
       surr(
         "#{",
         "}",
-        seq(
-          commaSep1($.relation_expression, false),
-          optional(seq("|", $.identifier)),
-        ),
+        seq(commaSep1($.relation_expression), optional(seq("|", $.identifier))),
       ),
 
     assignment: ($) =>
@@ -387,7 +378,7 @@ module.exports = grammar({
     enum_pattern: ($) =>
       seq(field("enum", $.ref), field("case", parens($.pattern))),
     record_pattern: ($) =>
-      braces(commaSep1($.identifier, false), optional(seq("|", $.identifier))),
+      braces(commaSep1($.identifier), optional(seq("|", $.identifier))),
 
     // Types (basic surface syntax)
     ref: ($) => choice($.identifier, $.path),
@@ -421,7 +412,7 @@ module.exports = grammar({
       prec.left(
         choice(
           $.type_ref,
-          braces(commaSep1($.type_ref, false)),
+          braces(commaSep1($.type_ref)),
           parensOptional(binary($, "+", $.eff_expr)),
           parensOptional(binary($, "-", $.eff_expr)),
         ),
@@ -468,8 +459,8 @@ module.exports = grammar({
     string_fragment: (_) => token.immediate(choice(/\\./, /[^"\\$]+/)),
 
     interpolated_string: ($) => {
-      const expr = surr("${", "}", field("expression", $.expression));
-      const fragment = field("fragment", $.string_fragment);
+      const expr = surr("${", "}", $.expression);
+      const fragment = $.string_fragment;
       return surr(
         '"',
         '"',
@@ -491,6 +482,12 @@ module.exports = grammar({
 
 // Helpers
 
+/** Modifiers, annotations, and doc comments
+ *
+ * @param {GrammarSymbols<string>} $ - The grammar symbols
+ * @param {...RuleOrLiteral} rules - The rules or literals to include after modifiers, annotations, and doc comments
+ * @returns {SeqRule} - The resulting rule with the modifiers, annotations, and doc comments
+ */
 function moddedSeq($, ...rules) {
   return seq(
     repeat($.doc_comment),
@@ -500,11 +497,29 @@ function moddedSeq($, ...rules) {
   );
 }
 
+/** Surround with open and close
+ *
+ * @param {RuleOrLiteral} open - The opening rule or literal
+ * @param {RuleOrLiteral} close - The closing rule or literal
+ * @param {...RuleOrLiteral} outerRules - The rules or literals to surround
+ * @returns {SeqRule} - The resulting rule for the surrounded content
+ */
 function surr(open, close, ...outerRules) {
   return seq(open, ...outerRules, close);
 }
 
-function sep1(rule, separator, trailing = false, precedence = (x) => x) {
+/** Separator with at least one element
+ *
+ * @param {RuleOrLiteral} rule - The rule for the elements in the list
+ * @param {RuleOrLiteral} separator - The rule for the separator
+ * @param {{ trailing?: boolean | 'any', precedence?: (x: any) => any }} options - Options for the list
+ * @returns {any} - The resulting rule for the separated list
+ */
+function sep1(
+  rule,
+  separator,
+  { trailing = false, precedence = (x) => x } = {},
+) {
   const rules = [rule, repeat(precedence(seq(separator, rule)))];
   switch (trailing) {
     case true: {
@@ -520,7 +535,18 @@ function sep1(rule, separator, trailing = false, precedence = (x) => x) {
   return precedence(seq(...rules));
 }
 
-function sep2(rule, separator, trailing = false, precedence = (x) => x) {
+/** Separator with at least two elements
+ *
+ * @param {RuleOrLiteral} rule - The rule for the elements in the list
+ * @param {RuleOrLiteral} separator - The rule for the separator
+ * @param {{ trailing?: boolean | 'any', precedence?: (x: any) => any }} options - Options for the list
+ * @returns {any} - The resulting rule for the separated list
+ */
+function sep2(
+  rule,
+  separator,
+  { trailing = false, precedence = (x) => x } = {},
+) {
   const rules = [rule, repeat1(precedence(seq(separator, rule)))];
   switch (trailing) {
     case true: {
@@ -536,11 +562,55 @@ function sep2(rule, separator, trailing = false, precedence = (x) => x) {
   return precedence(seq(...rules));
 }
 
-function commaSep1(rule, trailing = false, precedence = (x) => x) {
-  return sep1(rule, ",", trailing, precedence);
+/** Comma-separated list with at least one element
+ *
+ * @param {RuleOrLiteral} rule - The rule for the elements in the list
+ * @param {{ trailing?: boolean | 'any', precedence?: (x: any) => any }} options - Options for the list
+ * @returns {any} - The resulting rule for the comma-separated list
+ */
+function commaSep1(rule, { trailing = false, precedence = (x) => x } = {}) {
+  return sep1(rule, ",", { trailing, precedence });
 }
 
-// binary operation
+/** Binary expression helper
+ *
+ * @param {GrammarSymbols<string>} $ - The grammar symbols
+ * @param {RuleOrLiteral} op - The operator literal
+ * @param {RuleOrLiteral} next - The next expression rule
+ * @returns {SeqRule} - The resulting rule for the binary expression
+ */
 function binary($, op, next) {
   return seq(field("left", next), field("operator", op), field("right", next));
+}
+
+/** Helpers for tree-sitter grammar rules
+ * @param {(...rules: RuleOrLiteral[]) => Rule} fn - The rule function
+ * @returns {(...rules: RuleOrLiteral[]) => Rule} - The resulting rule function
+ */
+function makeWrapperOptional(fn) {
+  return (...rules) => choice(fn(...rules), seq(...rules));
+}
+
+/** Parens helper
+ * @param {...RuleOrLiteral} rules - The rules or literals to include inside the wrapper
+ * @returns {SeqRule} - The resulting rule for the wrapped content
+ */
+function parens(...rules) {
+  return surr("(", ")", ...rules);
+}
+
+/** Brackets helper
+ * @param {...RuleOrLiteral} rules - The rules or literals to include inside the wrapper
+ * @returns {SeqRule} - The resulting rule for the wrapped content
+ */
+function brackets(...rules) {
+  return surr("[", "]", ...rules);
+}
+
+/** Braces helper
+ * @param {...RuleOrLiteral} rules - The rules or literals to include inside the wrapper
+ * @returns {SeqRule} - The resulting rule for the wrapped content
+ */
+function braces(...rules) {
+  return surr("{", "}", ...rules);
 }
